1584649c17706d6adbc5868ed2eb2c10f0fc5ab4
TeamBerooz <102966405+TeamBerooz@users.noreply.github.com>
2022-04-06 22:36:35 +0430

    Add files via upload

---
+++ bot/Info-Bot.lua
@@ -0,0 +1,15 @@
+bot_token = "1180527087:AAFAg9AJFsSvAPk7gz4qfgue2vbH3MjAmhg" -- توکن رباتُ
+channel_username = '@BeroozTeam' -- آیدی کانال
+channel_inline = 'BeroozTeam' -- آیدی کانال بدون @
+sudo_username = '@GolPesareIran' -- آیدی سازنده ربات
+gp_sudo = -1001342888419 -- آیدی گروه سودو ها
+SUDO = 782187490 -- آیدی سودو اصلی
+BotSuperZedlink_id = 902464546 --آیدی ربات cli
+BotSuperZedlink_idapi = 1180527087 --آیدی ربات api
+RedisIndex = '1' -- شماره ردیس
+EndPm = " ツ"
+
+--[[
+opened By @BeroozTeam_Boy AND @BeroozTeam
+My Channel @BeroozTeam & @BeroozTeam
+]]--
